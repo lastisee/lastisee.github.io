70ca826cdab3a05d77b1e13a2212786d81ed12a8
lzzhanglin <zl950@foxmail.com>
2023-06-14 08:07:47 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(1)
   return (
     <div className="App">
       <div style={{margin: '80px auto',width: '200px', fontSize: '24px'}}>
         lastisee's website
+      </div>
+      <div style={{margin: '80px auto',width: '200px', fontSize: '24px'}}>
+        <span style={{margin: '12px'}}>count: {count}</span>
+        <button onClick={()=> setCount(prev => prev - 1)}>-</button>
+        <button onClick={()=> setCount(prev => prev + 1)}>+</button>
       </div>
     </div>
   );
